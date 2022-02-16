8e42fbbacb985d6226f1836580dd8f1c120fa939
greg_mac <greg_mac@greg-macui-MacBookPro.local>
2022-02-16 13:23:13 +0900

    first commit

---
+++ assets/c2iLicenseKey.js
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------------
+// C2i License Key summary report
+//
+// Product Name : C2i ^v2.5
+// License Type : Component License
+// Product No : All
+// Customer / Supplier: 청와대 / OO 시스템
+// Authenticated server
+//  -. Authenticated server #:  대
+//  -. Servers-IP:
+//  -. Domain: https://dashboard.jobs.go.kr/main
+// Expiration date : Unlimited
+//
+var c2i = c2i || {};
+c2i.licenseKey = '제공된 인증서에 기재된 아래 예제와 같은 일련번호를 모두 복사해 여기에 넣으세요.';
+// ex) c2i.licenseKey = 'C2I–STD–compName22–ghi99eurjd-26444548211154854814';
+// -----------------------------------------------------------------------------
---
+++ assets/data/bi/pyramidData.js
@@ -0,0 +1,27 @@
+/**
+ * Created by greg_mac on 2017. 1. 12..
+ */
+var pyramidData = [
+  { "areaNm": "서울특별시", "male": 43041, "female": 40852, "maleBefore": 40041, "femaleBefore": 42852 },
+  { "areaNm": "인천광역시", "male": 38867, "female": 36296, "maleBefore": 37867, "femaleBefore": 38296 },
+  { "areaNm": "부산", "male": 41748, "female": 40757, "maleBefore": 41048, "femaleBefore": 40057 },
+  { "areaNm": "대구", "male": 24831, "female": 23624, "maleBefore": 35031, "femaleBefore": 24624 },
+  { "areaNm": "광주", "male": 15764, "female": 15299, "maleBefore": 15264, "femaleBefore": 17299 },
+  { "areaNm": "대전광역시", "male": 17006, "female": 16071, "maleBefore": 17206, "femaleBefore": 18071 },
+  { "areaNm": "공주", "male": 24309, "female": 23235, "maleBefore": 24409, "femaleBefore": 26235 },
+  { "areaNm": "경기도", "male": 46756, "female": 46065, "maleBefore": 46356, "femaleBefore": 43065 },
+  { "areaNm": "경북", "male": 41923, "female": 41704, "maleBefore": 41023, "femaleBefore": 40704 },
+  { "areaNm": "경남", "male": 42565, "female": 42159, "maleBefore": 42065, "femaleBefore": 41159 },
+  { "areaNm": "전라북도", "male": 44316, "female": 45468, "maleBefore": 42316, "femaleBefore": 46468 },
+  { "areaNm": "전라남도", "male": 42975, "female": 44223, "maleBefore": 41975, "femaleBefore": 44223 },
+  { "areaNm": "충남", "male": 36755, "female": 39452, "maleBefore": 32755, "femaleBefore": 35452 },
+  { "areaNm": "충북", "male": 31578, "female": 34063, "maleBefore": 30578, "femaleBefore": 32063 },
+  { "areaNm": "강원", "male": 10328, "female": 11799, "maleBefore": 11328, "femaleBefore": 13799 },
+  { "areaNm": "제주특별자치시", "male": 13917, "female": 14949, "maleBefore": 13917, "femaleBefore": 18949 },
+  { "areaNm": "울릉", "male": 7920, "female": 8589, "maleBefore": 7020, "femaleBefore": 8989 },
+  { "areaNm": "울산", "male": 9003, "female": 10397, "maleBefore": 9803, "femaleBefore": 12397 },
+  { "areaNm": "영주", "male": 14322, "female": 16832, "maleBefore": 19322, "femaleBefore": 12832 },
+  { "areaNm": "속초", "male": 12369, "female": 15836, "maleBefore": 17369, "femaleBefore": 12836 },
+  { "areaNm": "해남", "male": 8710, "female": 12377, "maleBefore": 8410, "femaleBefore": 15377 },
+  { "areaNm": "목포", "male": 5853, "female": 12213, "maleBefore": 5653, "femaleBefore": 17213 }
+];
